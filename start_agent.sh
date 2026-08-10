87b1a300ebff3a1fe35b1c88e92eb98f22f259ce
vladisUl <vladis333@gmail.com>
2026-08-10 19:12:08 +0500

    Use global model environment in agent startup

--- start_agent.sh
+++ start_agent.sh
@@ -2,9 +2,10 @@
 
 set -e
 
+: "${CAT_AGENT_MODEL:?CAT_AGENT_MODEL is not set}"
+
 cd /opt/cat-agent
 
 export CAT_AGENT_API_BASE_URL=http://127.0.0.1:9380/v1
-export CAT_AGENT_MODEL='/opt/llama.cpp/models/gemma-4/gemma-4-E4B-it-Q4_K_M.gguf'
 
 exec python3 -m cat_agent.main
