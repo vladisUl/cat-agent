88531dafb49539f43b478a40eb661f4a0ac4021d
vladisUl <vladis333@gmail.com>
2026-08-31 19:19:10 +0500

    Test hard two-tone wake beep

--- tests/test_voice_tts.py
+++ tests/test_voice_tts.py
@@ -20,14 +20,22 @@ def _zero_crossings(pcm: bytes) -> int:
 
 
 class WakeBeepTest(unittest.TestCase):
-    def test_pcm_has_expected_duration_and_faded_edges(self) -> None:
+    def test_pcm_has_expected_duration_and_audio(self) -> None:
         pcm = _wake_beep_pcm()
         expected_frames = round(WAKE_BEEP_SAMPLE_RATE * WAKE_BEEP_SECONDS)
 
         self.assertEqual(len(pcm), expected_frames * 2)
-        self.assertEqual(struct.unpack_from("<h", pcm, 0)[0], 0)
-        self.assertEqual(struct.unpack_from("<h", pcm, len(pcm) - 2)[0], 0)
         self.assertNotEqual(pcm, b"\x00" * len(pcm))
+
+    def test_tones_start_without_fade(self) -> None:
+        pcm = _wake_beep_pcm()
+        midpoint = len(pcm) // 2
+
+        first_second_sample = struct.unpack_from("<h", pcm, 2)[0]
+        second_second_sample = struct.unpack_from("<h", pcm, midpoint + 2)[0]
+
+        self.assertGreater(abs(first_second_sample), 100)
+        self.assertGreater(abs(second_second_sample), 100)
 
     def test_second_tone_is_clearly_lower_than_first(self) -> None:
         pcm = _wake_beep_pcm()
